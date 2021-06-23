939b888e915778a20bbeecc857c5c059a627b132
NicoRosero <nicolas.nacho93@gmail.com>
2021-06-23 00:55:41 -0500

    2 commit

---
+++ PlanetMovileGame/Assets/Scripts/RandomPatrol.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPatrol : MonoBehaviour
+{
+
+  
+    public float minX;//-12
+    public float maxX;//12
+    public float minY;//-10.48
+    public float maxY;//-0.28
+
+    Vector2 targetPosition;
+    
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector2 GetRandomPosition()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+        return new Vector2(randomX,randomY);
+    }
+}
