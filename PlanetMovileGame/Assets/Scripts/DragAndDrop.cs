58610a604ac16c028e9abb83e07b12d44df9ec5d
NicoRosero <nicolas.nacho93@gmail.com>
2021-06-23 01:37:20 -0500

    added
    collider n physiscs to objects
    the drag and drop script with touch

---
+++ PlanetMovileGame/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DragAndDrop : MonoBehaviour
+{
+
+    bool moveAllowed;
+    Collider2D col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            //this gets the position touched into the screen into unity postition variables transformed from pixels
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            
+            //when we touch the screen for the first time
+            if (touch.phase==TouchPhase.Began)
+            {
+                Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
+                if (col == touchedCollider)
+                {
+                    moveAllowed = true;
+                }
+            }
+            //when our finger is still on the screen
+            if (touch.phase == TouchPhase.Moved)
+            {
+                if (moveAllowed)
+                {
+                    transform.position = new Vector2(touchPosition.x, touchPosition.y);
+                }
+            }
+            //when we take our finger off the screen
+            if (touch.phase == TouchPhase.Ended)
+            {
+                moveAllowed = false;
+            }
+
+
+
+        }
+
+
+
+    }
+}
--- PlanetMovileGame/Assets/Scripts/RandomPatrol.cs
+++ PlanetMovileGame/Assets/Scripts/RandomPatrol.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class RandomPatrol : MonoBehaviour
 {
 
@@ -12,18 +12,26 @@ public class RandomPatrol : MonoBehaviour
     public float maxY;//-0.28
 
     Vector2 targetPosition;
-    
+
+    public float speed;
     // Start is called before the first frame update
     
     void Start()
     {
-        
+        targetPosition = GetRandomPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if((Vector2)transform.position != targetPosition)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed*Time.deltaTime);
+        }
+        else
+        {
+            targetPosition = GetRandomPosition();
+        }
     }
 
     Vector2 GetRandomPosition()
@@ -32,4 +40,13 @@ public class RandomPatrol : MonoBehaviour
         float randomY = Random.Range(minY, maxY);
         return new Vector2(randomX,randomY);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Planets")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
 }
